3076e96b9d2a1342d71b021ba620050b8359bfaf
Viktor Kuzhelnyi <viktor@marketto.ru>
2021-06-15 10:39:47 +0500

    add gh-pages deploy env

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+module.exports = {
+  publicPath: process.env.DEPLOY_ENV === 'GH_PAGES'
+    ? '/admin-one-vue-tailwind/'
+    : '/'
+}
