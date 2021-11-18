9bb5be8e88b8d72f8920fcbed020ff2f30f6dcb8
Viktor Kuzhelnyi <viktor@marketto.ru>
2021-11-18 19:28:33 +0500

    fix centering

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,7 +1,7 @@
 <template>
   <footer v-show="isFooterBarVisible" class="bg-white py-2 px-6 dark:bg-gray-900">
     <level>
-      <div>
+      <div class="text-center md:text-left">
         <b>&copy; {{ year }}, <a href="https://justboil.me/" target="_blank">JustBoil.me</a>.</b>
         Get more with <a href="https://justboil.me/tailwind-admin-templates/vue-dashboard" target="_blank" class="text-blue-500">Premium version</a>
       </div>
