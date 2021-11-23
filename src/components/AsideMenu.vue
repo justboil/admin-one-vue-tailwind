744cc603d263482b050ee9ff77409614c37db96b
Viktor Kuzhelnyi <viktor@marketto.ru>
2021-11-23 20:36:44 +0500

    fix overflow

--- src/components/AsideMenu.vue
+++ src/components/AsideMenu.vue
@@ -3,7 +3,7 @@
     v-show="!isFullScreen"
     class="w-60 fixed top-0 z-40 h-screen bg-gray-800 transition-position lg:left-0
     scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-gray-600 scrollbar-track-gray-900
-    dark:scrollbar-track-gray-800 dark:bg-gray-900"
+    dark:scrollbar-track-gray-800 dark:bg-gray-900 overflow-auto"
     :class="[ isAsideMobileExpanded ? 'left-0' : '-left-60', isAsideLgActive ? 'block' : 'lg:hidden xl:block' ]"
   >
     <div class="flex flex-row w-full bg-gray-900 text-white flex-1 h-14 items-center">
