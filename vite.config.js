bed0fe9e7c58c9cd814561d31be7cc5653e1b678
Viktor Kuzhelnyi <viktor@marketto.ru>
2021-11-26 15:21:55 +0500

    add vitejs

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
+})
