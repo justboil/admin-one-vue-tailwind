3e1cebafdbc93e54012cac6e5581e6f3f5e9e293
Viktor Kuzhelnyi <viktor@marketto.ru>
2021-11-29 14:51:54 +0500

    upd configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,16 @@ module.exports = {
   env: {
     node: true
   },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly'
+  },
   extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
+    'plugin:vue/vue3-recommended'
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
