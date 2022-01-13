204f3d1378e49b4cd12aa25d4acfa06b35a48278
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-01-13 11:38:59 +0500

    upd: radius

--- src/components/ModalBox.vue
+++ src/components/ModalBox.vue
@@ -57,6 +57,7 @@ const cancel = () => confirmCancel('cancel')
       v-show="value"
       :title="title"
       class="shadow-lg w-full max-h-modal md:w-3/5 lg:w-2/5 z-50"
+      rounded="rounded-lg"
       :header-icon="mdiClose"
       @header-icon-click="cancel"
     >
