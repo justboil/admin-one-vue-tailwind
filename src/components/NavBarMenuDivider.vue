36ef4631b82a714a7adb7712a5bb5ce89672c1c2
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-01-13 16:25:08 +0500

    fix: height

--- src/components/NavBarMenuDivider.vue
+++ src/components/NavBarMenuDivider.vue
@@ -1,3 +1,3 @@
 <template>
-  <hr class="hidden lg:block lg:my-2 border-t border-gray-100 dark:border-gray-700">
+  <hr class="hidden lg:block lg:my-0.5 border-t border-gray-100 dark:border-gray-700">
 </template>
