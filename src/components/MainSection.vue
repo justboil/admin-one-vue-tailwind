897160010ee82b488ad3a62424d65e41f6c27cbe
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-01-15 01:50:09 +0500

    upd: classes

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,17 +1,5 @@
-<script setup>
-import { computed } from 'vue'
-import { useStore } from 'vuex'
-
-const store = useStore()
-
-const isFullScreen = computed(() => store.state.isFullScreen)
-</script>
-
 <template>
-  <section
-    class="px-0 md:px-6"
-    :class="[ isFullScreen ? 'flex h-screen items-center justify-center' : 'py-6' ]"
-  >
+  <section class="px-0 md:px-6 py-6">
     <slot />
   </section>
 </template>
