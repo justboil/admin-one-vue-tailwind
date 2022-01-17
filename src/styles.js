c2dc25ff97c99d1a82d69a8057d3bef2a35d15ae
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-01-18 03:00:16 +0500

    add: border, bg, table

--- src/styles.js
+++ src/styles.js
@@ -3,6 +3,8 @@ const bodyBase = 'text-base dark:bg-gray-800 dark:text-gray-100'
 export const basic = {
   html: '',
   body: `bg-gray-50 ${bodyBase}`,
+  lightBorder: 'border-gray-100',
+  lightBg: 'bg-gray-100',
   aside: 'bg-gray-800',
   asideBrand: 'bg-gray-900 text-white',
   asideMenuCloseLg: 'text-white',
@@ -15,12 +17,16 @@ export const basic = {
   navBarItemLabelHover: 'hover:text-blue-500',
   navBarItemLabelActiveColor: 'text-blue-600',
   navBarMenuListUpperLabel: 'bg-gray-100',
+  tableTr: 'lg:hover:bg-gray-100',
+  tableTrOdd: 'lg:bg-gray-50',
   overlay: 'from-gray-700 via-gray-900 to-gray-700'
 }
 
 export const white = {
   html: 'scrollbars-light',
   body: `bg-white ${bodyBase}`,
+  lightBorder: 'border-gray-100',
+  lightBg: 'bg-gray-100',
   aside: 'bg-white border-r border-gray-100',
   asideBrand: '',
   asideMenuCloseLg: '',
@@ -33,5 +39,7 @@ export const white = {
   navBarItemLabelHover: 'hover:text-black',
   navBarItemLabelActiveColor: 'text-black',
   navBarMenuListUpperLabel: 'bg-gray-50',
+  tableTr: 'lg:hover:bg-gray-100',
+  tableTrOdd: 'lg:bg-gray-50',
   overlay: 'from-white via-gray-100 to-white'
 }
