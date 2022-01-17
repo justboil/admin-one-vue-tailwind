2c7eee765f9e6f233dbd4fc5a49775964ec75caa
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-01-18 02:20:37 +0500

    upd: colors

--- src/colors.js
+++ src/colors.js
@@ -7,36 +7,36 @@ export const sectionBgErrorDark = `${sectionBgBase} from-pink-900 via-red-900 to
 export const colorsBg = {
   white: 'bg-white text-black',
   light: 'bg-gray-100 text-black',
-  success: 'bg-emerald-500 text-white',
-  danger: 'bg-red-500 text-white',
-  warning: 'bg-yellow-500 text-white',
+  success: 'bg-emerald-600 text-white',
+  danger: 'bg-red-600 text-white',
+  warning: 'bg-yellow-600 text-white',
   info: 'bg-blue-600 text-white'
 }
 
 export const colorsBgHover = {
   white: 'hover:bg-gray-50',
   light: 'hover:bg-gray-200',
-  success: 'hover:bg-emerald-600',
-  danger: 'hover:bg-red-600',
-  warning: 'hover:bg-yellow-600',
-  info: 'hover:bg-blue-500'
+  success: 'hover:bg-emerald-700',
+  danger: 'hover:bg-red-700',
+  warning: 'hover:bg-yellow-700',
+  info: 'hover:bg-blue-700'
 }
 
 export const colorsBorders = {
   white: 'border-gray-300',
   light: 'border-gray-200 dark:border-gray-400',
-  success: 'border-emerald-600',
-  danger: 'border-red-600',
-  warning: 'border-yellow-600',
+  success: 'border-emerald-700',
+  danger: 'border-red-700',
+  warning: 'border-yellow-700',
   info: 'border-blue-700'
 }
 
 export const colorsText = {
   white: 'text-black dark:text-gray-100',
   light: 'text-gray-700 dark:text-gray-400',
-  success: 'text-emerald-500',
-  danger: 'text-red-500',
-  warning: 'text-yellow-500',
+  success: 'text-emerald-600',
+  danger: 'text-red-600',
+  warning: 'text-yellow-600',
   info: 'text-blue-600'
 }
 
@@ -52,10 +52,10 @@ export const colorsOutline = {
 export const colorsOutlineHover = {
   white: 'hover:bg-gray-100 hover:text-gray-900 dark:hover:text-gray-900',
   light: 'hover:bg-gray-100 hover:text-gray-900 dark:hover:text-gray-900',
-  success: 'hover:bg-emerald-500 hover:text-white',
-  danger: 'hover:bg-red-500 hover:text-white',
-  warning: 'hover:bg-yellow-500 hover:text-white',
-  info: 'hover:bg-blue-600 hover:text-white'
+  success: 'hover:bg-emerald-700 hover:text-white',
+  danger: 'hover:bg-red-700 hover:text-white',
+  warning: 'hover:bg-yellow-700 hover:text-white',
+  info: 'hover:bg-blue-700 hover:text-white'
 }
 
 export const getButtonColor = (color, isOutlined, hasHover) => {
