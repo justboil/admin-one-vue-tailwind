2f174340b74e08655624f192fe663937ecc453db
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-01-18 02:26:43 +0500

    upd: navbar/normal colors

--- src/components/Divider.vue
+++ src/components/Divider.vue
@@ -13,6 +13,7 @@ const lightBorderStyle = computed(() => store.state.lightBorderStyle)
 
 <template>
   <hr
-    :class="[lightBorderStyle, props.navBar ? 'hidden lg:block lg:my-0.5' : 'my-6 -mx-6']"
-    class="border-t dark:border-gray-700">
+    :class="[lightBorderStyle, props.navBar ? 'hidden lg:block lg:my-0.5 dark:border-gray-700' : 'my-6 -mx-6 dark:border-gray-800']"
+    class="border-t"
+  >
 </template>
