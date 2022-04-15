1a1b70dcd584bd3d7910c68004a8a267fa09d678
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-04-15 22:29:13 +0500

    add: pinia store

---
+++ src/stores/main.js
@@ -0,0 +1,124 @@
+import { defineStore } from 'pinia'
+import * as styles from '@/styles'
+import { darkModeKey, styleKey } from '@/config'
+import axios from 'axios'
+
+export const useMainStore = defineStore('root', {
+  state: () => ({
+    /* Styles */
+    lightBorderStyle: '',
+    lightBgStyle: '',
+    asideStyle: '',
+    asideBrandStyle: '',
+    asideMenuCloseLgStyle: '',
+    asideMenuLabelStyle: '',
+    asideMenuItemStyle: '',
+    asideMenuItemActiveStyle: '',
+    asideMenuItemInactiveStyle: '',
+    asideSubmenuListStyle: '',
+    navBarItemLabelStyle: '',
+    navBarItemLabelHoverStyle: '',
+    navBarItemLabelActiveColorStyle: '',
+    navBarMenuListUpperLabelStyle: '',
+    tableTrStyle: '',
+    tableTrOddStyle: '',
+    overlayStyle: '',
+
+    /* User */
+    userName: null,
+    userEmail: null,
+    userAvatar: null,
+
+    /* fullScreen - fullscreen form layout (e.g. login page) */
+    isFullScreen: true,
+
+    /* Aside */
+    isAsideMobileExpanded: false,
+    isAsideLgActive: false,
+
+    /* Dark mode */
+    darkMode: false,
+
+    /* Field focus with ctrl+k (to register only once) */
+    isFieldFocusRegistered: false,
+
+    /* Sample data (commonly used) */
+    clients: [],
+    history: []
+  }),
+  actions: {
+    setUser (payload) {
+      if (payload.name) {
+        this.userName = payload.name
+      }
+      if (payload.email) {
+        this.userEmail = payload.email
+      }
+      if (payload.avatar) {
+        this.userAvatar = payload.avatar
+      }
+    },
+
+    setStyle (payload) {
+      const style = styles[payload] ?? styles.basic
+
+      document.body.className = style.body
+      document.documentElement.className = style.html
+
+      if (localStorage[styleKey] !== payload) {
+        localStorage.setItem(styleKey, payload)
+      }
+
+      for (const key in style) {
+        if (['body', 'html'].includes(key)) {
+          continue
+        }
+
+        this[`${key}Style`] = style[key]
+      }
+    },
+
+    asideMobileToggle (payload = null) {
+      const isShow = payload !== null ? payload : !this.isAsideMobileExpanded
+
+      document.getElementById('app').classList[isShow ? 'add' : 'remove']('ml-60', 'lg:ml-0')
+
+      document.documentElement.classList[isShow ? 'add' : 'remove']('m-clipped')
+
+      this.isAsideMobileExpanded = isShow
+    },
+
+    asideLgToggle (payload = null) {
+      this.isAsideLgActive = payload !== null ? payload : !this.isAsideLgActive
+    },
+
+    fullScreenToggle (payload) {
+      this.isFullScreen = payload
+
+      document.documentElement.classList[payload ? 'add' : 'remove']('full-screen')
+    },
+
+    darkMode (payload = null) {
+      const value = payload !== null ? payload : !this.darkMode
+
+      document.documentElement.classList[value ? 'add' : 'remove']('dark')
+
+      localStorage.setItem(darkModeKey, value ? '1' : '0')
+
+      this.darkMode = value
+    },
+
+    fetch (sampleDataKey) {
+      axios
+        .get(`data-sources/${sampleDataKey}.json`)
+        .then(r => {
+          if (r.data && r.data.data) {
+            this[sampleDataKey] = r.data.data
+          }
+        })
+        .catch(error => {
+          alert(error.message)
+        })
+    }
+  }
+})
