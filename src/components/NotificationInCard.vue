3a5b059c75c24c4954789db15dea4956a218aceb
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-06-15 14:39:18 +0500

    add: `NotificationInCard`

---
+++ src/components/NotificationInCard.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from 'vue'
+import { colorsBg, colorsBorders } from '@/colors.js'
+
+const props = defineProps({
+  color: {
+    type: String,
+    required: true
+  }
+})
+
+const componentClass = computed(() => [colorsBg[props.color], colorsBorders[props.color]])
+</script>
+
+<template>
+  <div class="flex flex-col mb-6 -mt-6 -mr-6 -ml-6">
+    <div
+      :class="componentClass"
+      class="flex flex-col -m-px p-6 rounded-t-lg"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
