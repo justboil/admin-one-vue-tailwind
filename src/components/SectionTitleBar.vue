fc34b8dab97c0c04d8573d1f5afc451d7e3b467b
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-07-22 14:05:37 +0500

    upd: css class for section-title-bar-stack-item

--- src/components/SectionTitleBar.vue
+++ src/components/SectionTitleBar.vue
@@ -18,7 +18,7 @@ defineProps({
         <li
           v-for="(title, index) in titleStack"
           :key="index"
-          class="stack-item inline-block pr-3 text-2xl text-gray-500 dark:text-gray-400 last:pr-0 last:font-black last:text-black"
+          class="section-title-bar-stack-item inline-block pr-3 text-2xl text-gray-500 dark:text-gray-400 last:pr-0 last:font-black last:text-black"
         >
           {{ title }}
         </li>
