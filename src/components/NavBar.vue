29005c1cc83c0a05f4347873951e802cb7ed50d8
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-08-12 14:07:43 +0500

    upd: refactor `NavBar`

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,199 +1,87 @@
 <script setup>
-import { computed, ref } from 'vue'
-import { containerMaxW } from '@/config.js'
-import { useMainStore } from '@/stores/main.js'
-import { useStyleStore } from '@/stores/style.js'
-import { useLayoutStore } from '@/stores/layout.js'
+import { ref } from 'vue'
 import {
   mdiForwardburger,
   mdiBackburger,
   mdiClose,
   mdiDotsVertical,
   mdiMenu,
-  mdiClockOutline,
-  mdiCloud,
-  mdiCrop,
-  mdiAccount,
-  mdiCogOutline,
-  mdiEmail,
-  mdiLogout,
-  mdiGithub,
-  mdiThemeLightDark
 } from '@mdi/js'
-import NavBarItem from '@/components/NavBarItem.vue'
-import NavBarItemLabel from '@/components/NavBarItemLabel.vue'
-import NavBarMenu from '@/components/NavBarMenu.vue'
-import BaseDivider from '@/components/BaseDivider.vue'
-import UserAvatarCurrentUser from '@/components/UserAvatarCurrentUser.vue'
+import { containerMaxW } from '@/config.js'
+import { useLayoutStore } from '@/stores/layout.js'
 import BaseIcon from '@/components/BaseIcon.vue'
-import NavBarSearch from '@/components/NavBarSearch.vue'
+import FormControl from '@/components/FormControl.vue'
+import NavBarMenuList from '@/components/NavBarMenuList.vue'
+import NavBarItemPlain from '@/components/NavBarItemPlain.vue'
 
-const mainStore = useMainStore()
+defineProps({
+  menu: {
+    type: Array,
+    required: true
+  }
+})
 
-const userName = computed(() => mainStore.userName)
+const emit = defineEmits(['menu-click'])
 
-const styleStore = useStyleStore()
-
-const toggleLightDark = () => {
-  styleStore.setDarkMode()
+const menuClick = (event, item) => {
+  emit('menu-click', event, item)
 }
 
 const layoutStore = useLayoutStore()
 
 const isMenuNavBarActive = ref(false)
-
-const menuNavBarToggle = () => {
-  isMenuNavBarActive.value = !isMenuNavBarActive.value
-}
-
-const menuOpenLg = () => {
-  layoutStore.isAsideLgActive = true
-}
-
-const logout = () => {
-  //
-}
 </script>
 
 <template>
-  <nav
-    class="top-0 left-0 right-0 fixed bg-gray-50 h-14 z-30 w-screen transition-position lg:w-auto dark:bg-slate-800"
-  >
+  <nav class="top-0 inset-x-0 fixed bg-gray-50 h-14 z-30 transition-position w-screen lg:w-auto dark:bg-slate-800">
     <div
       class="flex lg:items-stretch"
       :class="containerMaxW"
     >
-      <div class="flex-1 items-stretch flex h-14">
-        <NavBarItem
-          type="flex lg:hidden"
+      <div class="flex flex-1 items-stretch h-14">
+        <NavBarItemPlain
+          display="flex lg:hidden"
           @click.prevent="layoutStore.asideMobileToggle()"
         >
           <BaseIcon
             :path="layoutStore.isAsideMobileExpanded ? mdiBackburger : mdiForwardburger"
             size="24"
           />
-        </NavBarItem>
-        <NavBarItem
-          type="hidden lg:flex xl:hidden"
-          @click.prevent="menuOpenLg"
+        </NavBarItemPlain>
+        <NavBarItemPlain
+          display="hidden lg:flex xl:hidden"
+          @click.prevent="layoutStore.isAsideLgActive = true"
         >
           <BaseIcon
             :path="mdiMenu"
             size="24"
           />
-        </NavBarItem>
-        <NavBarItem>
-          <NavBarSearch />
-        </NavBarItem>
+        </NavBarItemPlain>
+        <NavBarItemPlain use-margin>
+          <FormControl
+            placeholder="Search (ctrl+k)"
+            ctrl-k-focus
+            transparent
+            borderless
+          />
+        </NavBarItemPlain>
       </div>
       <div class="flex-none items-stretch flex h-14 lg:hidden">
-        <NavBarItem @click.prevent="menuNavBarToggle">
+        <NavBarItemPlain @click.prevent="isMenuNavBarActive = !isMenuNavBarActive">
           <BaseIcon
             :path="isMenuNavBarActive ? mdiClose : mdiDotsVertical"
             size="24"
           />
-        </NavBarItem>
+        </NavBarItemPlain>
       </div>
       <div
-        class="absolute w-screen top-14 left-0 bg-gray-50 shadow
-          lg:w-auto lg:items-stretch lg:flex lg:grow lg:static lg:border-b-0 lg:overflow-visible lg:shadow-none dark:bg-slate-800"
+        class="max-h-screen-menu overflow-y-auto lg:overflow-visible absolute w-screen top-14 left-0 bg-gray-50 shadow-lg lg:w-auto lg:flex lg:static lg:shadow-none dark:bg-slate-800"
         :class="[isMenuNavBarActive ? 'block' : 'hidden']"
       >
-        <div
-          class="max-h-screen-menu overflow-y-auto lg:overflow-visible lg:flex lg:items-stretch lg:justify-end lg:ml-auto"
-        >
-          <NavBarMenu>
-            <NavBarItemLabel
-              :icon="mdiMenu"
-              label="Sample menu"
-            />
-
-            <template #dropdown>
-              <NavBarItem>
-                <NavBarItemLabel
-                  :icon="mdiClockOutline"
-                  label="Item One"
-                />
-              </NavBarItem>
-              <NavBarItem>
-                <NavBarItemLabel
-                  :icon="mdiCloud"
-                  label="Item Two"
-                />
-              </NavBarItem>
-              <BaseDivider nav-bar />
-              <NavBarItem>
-                <NavBarItemLabel
-                  :icon="mdiCrop"
-                  label="Item Last"
-                />
-              </NavBarItem>
-            </template>
-          </NavBarMenu>
-          <NavBarMenu>
-            <NavBarItemLabel :label="userName">
-              <UserAvatarCurrentUser class="w-6 h-6 mr-3 inline-flex" />
-            </NavBarItemLabel>
-
-            <template #dropdown>
-              <NavBarItem to="/profile">
-                <NavBarItemLabel
-                  :icon="mdiAccount"
-                  label="My Profile"
-                />
-              </NavBarItem>
-              <NavBarItem>
-                <NavBarItemLabel
-                  :icon="mdiCogOutline"
-                  label="Settings"
-                />
-              </NavBarItem>
-              <NavBarItem>
-                <NavBarItemLabel
-                  :icon="mdiEmail"
-                  label="Messages"
-                />
-              </NavBarItem>
-              <BaseDivider nav-bar />
-              <NavBarItem>
-                <NavBarItemLabel
-                  :icon="mdiLogout"
-                  label="Log Out"
-                />
-              </NavBarItem>
-            </template>
-          </NavBarMenu>
-          <NavBarItem
-            is-desktop-icon-only
-            @click.prevent="toggleLightDark"
-          >
-            <NavBarItemLabel
-              :icon="mdiThemeLightDark"
-              label="Light/Dark"
-              is-desktop-icon-only
-            />
-          </NavBarItem>
-          <NavBarItem
-            href="https://github.com/justboil/admin-one-vue-tailwind"
-            is-desktop-icon-only
-          >
-            <NavBarItemLabel
-              :icon="mdiGithub"
-              label="GitHub"
-              is-desktop-icon-only
-            />
-          </NavBarItem>
-          <NavBarItem
-            is-desktop-icon-only
-            @click="logout"
-          >
-            <NavBarItemLabel
-              :icon="mdiLogout"
-              label="Log out"
-              is-desktop-icon-only
-            />
-          </NavBarItem>
-        </div>
+        <NavBarMenuList
+          :menu="menu"
+          @menu-click="menuClick"
+        />
       </div>
     </div>
   </nav>
--- src/components/NavBarItem.vue
+++ src/components/NavBarItem.vue
@@ -1,36 +1,29 @@
 <script setup>
-import { useStyleStore } from '@/stores/style.js'
+import { mdiChevronUp, mdiChevronDown } from '@mdi/js'
 import { RouterLink } from 'vue-router'
-import { computed } from 'vue'
+import { computed, ref, onMounted, onBeforeUnmount } from 'vue'
+import { useStyleStore } from '@/stores/style.js'
+import { useMainStore } from '@/stores/main.js'
+import BaseIcon from '@/components/BaseIcon.vue'
+import UserAvatarCurrentUser from '@/components/UserAvatarCurrentUser.vue'
+import NavBarMenuList from '@/components/NavBarMenuList.vue'
+import BaseDivider from '@/components/BaseDivider.vue'
 
 const props = defineProps({
-  href: {
-    type: String,
-    default: null
-  },
-  to: {
-    type: String,
-    default: null
-  },
-  type: {
-    type: String,
-    default: 'flex'
-  },
-  activeColor: {
-    type: String,
-    default: null
-  },
-  isDesktopIconOnly: Boolean,
-  dropdown: Boolean,
-  active: Boolean
+  item: {
+    type: Object,
+    required: true
+  }
 })
 
+const emit = defineEmits(['menu-click'])
+
 const is = computed(() => {
-  if (props.href) {
+  if (props.item.href) {
     return 'a'
   }
 
-  if (props.to) {
+  if (props.item.to) {
     return RouterLink
   }
 
@@ -39,42 +32,106 @@ const is = computed(() => {
 
 const styleStore = useStyleStore()
 
-const activeColor = props.activeColor ?? `${styleStore.navBarItemLabelActiveColorStyle} dark:text-slate-400`
-
 const componentClass = computed(() => {
   const base = [
-    props.type,
-    props.active
-      ? activeColor
-      : `${styleStore.navBarItemLabelStyle} dark:text-white dark:hover:text-slate-400 ${styleStore.navBarItemLabelHoverStyle}`
+    isDropdownActive.value
+      ? `${styleStore.navBarItemLabelActiveColorStyle} dark:text-slate-400`
+      : `${styleStore.navBarItemLabelStyle} dark:text-white dark:hover:text-slate-400 ${styleStore.navBarItemLabelHoverStyle}`,
+    props.item.menu ? 'lg:py-2 lg:px-3' : 'py-2 px-3'
   ]
 
-  if (props.type === 'block') {
-    base.push('lg:flex')
-  }
-
-  if (!props.dropdown) {
-    base.push('py-2', 'px-3')
-  } else {
-    base.push('p-0', 'lg:py-2', 'lg:px-3')
-  }
-
-  if (props.isDesktopIconOnly) {
+  if (props.item.isDesktopNoLabel) {
     base.push('lg:w-16', 'lg:justify-center')
   }
 
   return base
 })
+
+const mainStore = useMainStore()
+
+const isDropdownActive = ref(false)
+
+const menuClick = event => {
+  emit('menu-click', event, props.item)
+
+  if (props.item.menu) {
+    isDropdownActive.value = !isDropdownActive.value
+  }
+}
+
+const menuClickDropdown = (event, item) => {
+  emit('menu-click', event, item)
+}
+
+const root = ref(null)
+
+const forceClose = event => {
+  if (root.value && !root.value.contains(event.target)) {
+    isDropdownActive.value = false
+  }
+}
+
+onMounted(() => {
+  if (props.item.menu) {
+    window.addEventListener('click', forceClose)
+  }
+})
+
+onBeforeUnmount(() => {
+  if (props.item.menu) {
+    window.removeEventListener('click', forceClose)
+  }
+})
 </script>
 
 <template>
+  <BaseDivider
+    v-if="item.isDivider"
+    nav-bar 
+  />
   <component
     :is="is"
-    class="items-center grow-0 shrink-0 relative cursor-pointer"
+    v-else
+    ref="root"
+    class="block lg:flex items-center relative cursor-pointer"
     :class="componentClass"
-    :to="to"
-    :href="href"
+    :to="item.to ?? null"
+    :href="item.href ?? null"
+    @click="menuClick"
   >
-    <slot />
+    <div
+      class="flex items-center"
+      :class="{ 'bg-gray-100 dark:bg-slate-800 lg:bg-transparent lg:dark:bg-transparent p-3 lg:p-0': item.menu }"
+    >
+      <UserAvatarCurrentUser
+        v-if="item.isCurrentUser"
+        class="w-6 h-6 mr-3 inline-flex"
+      />
+      <BaseIcon
+        v-if="item.icon"
+        :path="item.icon"
+        class="transition-colors"
+      />
+      <span
+        class="px-2 transition-colors"
+        :class="{ 'lg:hidden': item.isDesktopNoLabel && item.icon }"
+      >{{ item.isCurrentUser ? mainStore.userName : item.label }}</span>
+      <BaseIcon
+        v-if="item.menu"
+        :path="isDropdownActive ? mdiChevronUp : mdiChevronDown"
+        class="hidden lg:inline-flex transition-colors"
+      />
+    </div>
+    <div
+      v-if="item.menu"
+      class="text-sm border-b border-gray-100 lg:border lg:bg-white lg:absolute
+        lg:top-full lg:left-0 lg:min-w-full lg:z-20 lg:rounded-lg lg:shadow-lg lg:dark:bg-slate-800 dark:border-slate-700"
+      :class="{ 'lg:hidden': !isDropdownActive }"
+    >
+      <NavBarMenuList
+        :menu="item.menu"
+        @menu-click="menuClickDropdown"
+      />
+    </div>
   </component>
 </template>
