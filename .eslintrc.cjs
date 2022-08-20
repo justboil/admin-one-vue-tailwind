536b957030eb2bd50a21220808a03a0880a5dbf8
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-08-20 12:29:12 +0500

    upd: package; fix: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,13 @@
 /* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
   "root": true,
   "extends": [
     "plugin:vue/vue3-recommended",
     "eslint:recommended"
-  ]
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
 }
