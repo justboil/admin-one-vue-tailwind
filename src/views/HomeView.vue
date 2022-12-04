1992baf2e4d7ce7dac91eabb3a6a64494ac59d7a
Wonyeong Choi <coma01155@gmail.com>
2022-12-04 23:21:44 +0900

    홈 화면 수정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@ import {
   mdiMonitorCellphone,
   mdiReload,
   mdiGithub,
-  mdiChartPie,
+  mdiChartPie
 } from "@mdi/js";
 import * as chartConfig from "/src/components/Charts/chart.config.js";
 import LineChart from "/src/components/Charts/LineChart.vue";
@@ -24,6 +24,7 @@ import LayoutAuthenticated from "/src/layouts/LayoutAuthenticated.vue";
 import SectionTitleLineWithButton from "/src/components/SectionTitleLineWithButton.vue";
 import SectionBannerStarOnGitHub from "/src/components/SectionBannerStarOnGitHub.vue";
 import { useUserStore } from "../stores/user";
+import BaseDivider from "/src/components/BaseDivider.vue";
 
 const chartData = ref(null);
 
@@ -46,8 +47,13 @@ const transactionBarItems = computed(() => mainStore.history);
 <template>
   <LayoutAuthenticated>
     <SectionMain>
-
-
+      <CardBox :class="cardClass">
+        <BaseButton label="프로필" to="/profile"></BaseButton>
+      </CardBox>
+      <BaseDivider/>
+      <CardBox :class="cardClass">
+        <BaseButton label="스터디 관리" to="/studygroup"></BaseButton>
+      </CardBox>
 
     </SectionMain>
   </LayoutAuthenticated>
