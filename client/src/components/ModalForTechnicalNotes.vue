37bf05d584b9ed1f2f35bc11d20c4b448ce0a530
Judit Calero <judit.calero84@gmail.com>
2024-02-22 12:09:50 +0100

    ready for backend scaffolding

--- client/src/components/ModalForTechnicalNotes.vue
+++ client/src/components/ModalForTechnicalNotes.vue
@@ -3,6 +3,7 @@
 import { ref, defineProps, defineEmits, computed } from 'vue';
 import modalContent from '@/modalTechnicalContents.js'
 
+
 const props = defineProps({ modalIsVisible: Boolean, modalButtonName: String })
 
 const modalBodyToDisplay = computed(() => {
@@ -55,7 +56,7 @@ function closeModalClick(){
               <!-- Modal body -->
               <div class="p-4 md:p-5 space-y-4">
                   <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                      {{ modalBodyToDisplay }}
+                      <div v-html="modalBodyToDisplay"></div>
                   </p>
                   
               </div>
--- client/src/modalTechnicalContents.js
+++ client/src/modalTechnicalContents.js
@@ -1,8 +1,14 @@
+import MarkdownIt from "markdown-it";
+
+const markdown = new MarkdownIt();
+
+markdown.render("# Header");
+
 
 export default [
     {
       name: 'global_graph',            
-      content: 'Global graph notes.',
+      content: markdown.render("# Header body. <br> another thing."),
     },
     {
         name: 'global_earnings',            
