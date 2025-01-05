e0062421e919f33abb3ac4efec27c5b9c6fe3489
menjatot <menjatot@gmail.com>
2025-01-05 23:19:24 +0100

    jan-05

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/admin-one-vue-tailwind/",
+  // base: "/admin-one-vue-tailwind/",
+  base: "./",
   plugins: [vue() ],
   define: {
     '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': false,
