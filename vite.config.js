9b844360f965b29faec0f790293668d210e8d427
vikdiesel <viktor@marketto.ru>
2025-03-04 21:39:55 +0500

    fix: missing base

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/admin-one-vue-tailwind/",
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
