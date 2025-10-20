370143ca39aa6622f683cc5a92a8bb7c95344108
vikdiesel <viktor@marketto.ru>
2025-10-20 08:51:00 +0000

    Deploying to gh-pages from @ justboil/admin-one-vue-tailwind@60f48ca5b75217fb9d88392f25f7d135ed1a6b8e 🚀

--- assets/CardBoxComponentEmpty-BIOzWf94.js
+++
@@ -1,1 +0,0 @@
-import{a1 as t,p as o,o as r,i as s}from"./index-C76QD15C.js";const n={},a={class:"text-center py-24 text-gray-500 dark:text-slate-400"};function c(p,e){return r(),o("div",a,[...e[0]||(e[0]=[s("p",null,"Nothing's here…",-1)])])}const d=t(n,[["render",c]]);export{d as C};
