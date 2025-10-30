876daab187c3cf56bc6cf5517be563cbd74d4c6b
vikdiesel <viktor@marketto.ru>
2025-10-30 13:47:58 +0500

    add: error logic to FormField

--- src/components/FormField.vue
+++ src/components/FormField.vue
@@ -2,18 +2,10 @@
 import { computed, useSlots } from 'vue'
 
 defineProps({
-  label: {
-    type: String,
-    default: null,
-  },
-  labelFor: {
-    type: String,
-    default: null,
-  },
-  help: {
-    type: String,
-    default: null,
-  },
+  label: String,
+  labelFor: String,
+  help: String,
+  error: String,
 })
 
 const slots = useSlots()
@@ -36,12 +28,22 @@ const wrapperClass = computed(() => {
 
 <template>
   <div class="mb-6 last:mb-0">
-    <label v-if="label" :for="labelFor" class="mb-2 block font-bold">{{ label }}</label>
+    <label
+      v-if="label"
+      :for="labelFor"
+      class="mb-2 block font-bold"
+      :class="{ 'text-red-600 dark:text-red-500': !!error }"
+      >{{ label }}</label
+    >
     <div :class="wrapperClass">
       <slot />
     </div>
-    <div v-if="help" class="mt-1 text-xs text-gray-500 dark:text-slate-400">
-      {{ help }}
+    <div
+      v-if="help || error"
+      class="mt-1 text-xs"
+      :class="error ? 'text-red-600 dark:text-red-500' : 'text-gray-500 dark:text-slate-400'"
+    >
+      {{ error ?? help }}
     </div>
   </div>
 </template>
