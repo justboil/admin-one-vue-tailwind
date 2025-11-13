cee42a13b7ea58532d1669589d3b6e27582f8966
vikdiesel <viktor@marketto.ru>
2025-11-13 13:51:23 +0500

    add: CardBoxMessage

---
+++ src/components/CardBoxMessage.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  isSmall: Boolean,
+})
+</script>
+
+<template>
+  <div
+    class="text-gray-500 last:mb-0 dark:text-slate-400"
+    :class="isSmall ? 'mb-3 text-sm' : 'mb-6'"
+  >
+    <slot />
+  </div>
+</template>
